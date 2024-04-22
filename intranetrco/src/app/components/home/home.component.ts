97c407cbc5c24b43d5ad9c7ea09a42569fd4fc8f
Lary SENE <lary.sene@canal-plus.com>
2024-04-22 16:56:53 -0400

    Home components update with inscription

--- intranetrco/src/app/components/home/home.component.ts
+++ intranetrco/src/app/components/home/home.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MesInscriptionsComponent } from '../mes-inscriptions/mes-inscriptions.component';
+import { InscriptionsComponent } from '../inscriptions/inscriptions.component';
+import { InscriptionsService } from '../../core/services/inscriptions.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MesInscriptionsComponent,InscriptionsComponent,AsyncPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.less'
 })
-export class HomeComponent {
+export class HomeComponent  {
+
+  private readonly inscription = inject(InscriptionsService);
+  mesinscriptions = this.inscription.getMesInscriptions();
 
 }
