d3c08b0f65d8eb25cb3accad27e23fb169900a99
Lary SENE <lary.sene@canal-plus.com>
2024-04-22 22:53:03 -0400

    accordion mes inscription

--- intranetrco/src/app/components/mes-inscriptions/mes-inscriptions.component.ts
+++ intranetrco/src/app/components/mes-inscriptions/mes-inscriptions.component.ts
@@ -1,14 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { Inscription } from '../../core/models/models';
 import { NgFor,DatePipe } from '@angular/common';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-mes-inscriptions',
   standalone: true,
-  imports: [NgFor,DatePipe],
+  imports: [NgFor,DatePipe,MatExpansionModule,MatIcon],
   templateUrl: './mes-inscriptions.component.html',
   styleUrl: './mes-inscriptions.component.less'
 })
 export class MesInscriptionsComponent {
   @Input() inscriptions: Inscription[] | null= null;
+  panelOpenState = false;
 }
