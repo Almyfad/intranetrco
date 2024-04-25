1041c3812bb25c623569b5867dc2c4408091bfdb
Lary SENE <lary.sene@canal-plus.com>
2024-04-24 21:57:15 -0400

    inscription form

--- intranetrco/src/app/components/conference-expansion-panel/conference-expansion-panel.component.ts
+++ intranetrco/src/app/components/conference-expansion-panel/conference-expansion-panel.component.ts
@@ -1,20 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIcon } from '@angular/material/icon';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
-import { Centre, Conference, Inscription } from '../../core/models/models';
+import { Conference, Inscription } from '../../core/models/models';
 import { DatePipe, NgIf } from '@angular/common';
 import { SkeletonValue } from '../../core/class/skeletonvalue';
+import { MatButton } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'conference-expansion-panel',
   standalone: true,
-  imports: [MatExpansionModule, NgxSkeletonLoaderModule, MatIcon, DatePipe, NgIf],
+  imports: [MatExpansionModule, NgxSkeletonLoaderModule,
+    ReactiveFormsModule, MatSelectModule,
+    MatIcon, DatePipe, NgIf, MatButton, MatInputModule],
   templateUrl: './conference-expansion-panel.component.html',
   styleUrl: './conference-expansion-panel.component.less'
 })
 export class ConferenceExpansionPanelComponent {
   @Input() SklValue!: SkeletonValue<Conference | Inscription>;
+  private builder: FormBuilder = inject(FormBuilder)
+
+
 
   get isInscription(): boolean {
     let obj = this.SklValue.value;
@@ -33,7 +42,49 @@ export class ConferenceExpansionPanelComponent {
       return (this.SklValue.value as Inscription).conference;
     }
 
-  }
 
+  }
+  get isEditable(): boolean { return this.isInscription }
+  get isNew(): boolean { return this.isConference }
+
+
+
+
+  heuresArriver = ["Vendredi soir (Repas + nuit)",
+    "Vendredi soir (Nuit)",
+    "Samedi matin",
+    "Samedi soir (Repas + nuit)",
+    "Samedi soir (Nuit)",
+    "Dimanche matin",
+    "Autre (préciser)"]
+
+  heuresDepart = ["Samedi fin CR",
+    "Dimanche fin CR",
+    "Autre (préciser)"]
+
+  participation = ["Si besoin",
+    "Oui",
+    "Non"]
+
+  lit = ["Indifférent",
+    "inférieur",
+    "supérieur",
+    "Pas de lit"]
+
+
+    propDescription: FormControl = new FormControl<string>('', [])
+    heureArriverControl: FormControl = new FormControl<string>('', [])
+    heureDepartControl: FormControl = new FormControl<string>('', [])
+    participationControl: FormControl = new FormControl<string>(this.participation[0], [])
+    litControl: FormControl = new FormControl<string>(this.lit[0], [])
+
+  form: FormGroup = this.builder.group({
+    description: this.propDescription,
+  })
+
+
+  subscribe() {
+    if (this.form.invalid) return
+  }
 
 }
